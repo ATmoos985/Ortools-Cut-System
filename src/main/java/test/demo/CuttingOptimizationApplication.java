6483e5b32991955295a3634d2589525a2564820b
Tmoos <krmoos985@gmail.com>
2026-05-02 20:32:33 +0800

    feat：回到一开始的分支

--- src/main/java/test/demo/CuttingOptimizationApplication.java
+++ src/main/java/test/demo/CuttingOptimizationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//newtest
 @SpringBootApplication
 public class CuttingOptimizationApplication implements WebMvcConfigurer {
 
